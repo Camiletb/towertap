0f1bc23bbb5d47fadefedf7dee71d27142c08fc5
Camiletb <caena@alumni.uv.es>
2022-05-06 13:12:33 +0200

    feat: bloque y cámara colocados

--- juego.js
+++ juego.js
@@ -1,32 +1,61 @@
-//Creando escena
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+// ------------------------------------------------
+// BASIC SETUP
+// ------------------------------------------------
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+// Crear una escena vacía
+var scene = new THREE.Scene();
+
+// Cámara
+const aspect = window.innerWidth / window.innerHeight;
+const width = 20;
+const height = width / aspect;
+const camera = new THREE.OrthographicCamera(
+    // izq, der, arriba, abajo, plano cercano, plano lejano
+    width / -2, 
+    width / 2, 
+    height / 2, 
+    height / -2, 
+    1, 
+    1000 );
+camera.position.set(10, 5, 10);
+camera.lookAt(scene.position);
+
+// Configurar el render
+var renderer = new THREE.WebGLRenderer({antialias:true}); // Crear con Antialiasing
+renderer.setClearColor("#000000"); // Configurar el clear color del render
+renderer.setSize( window.innerWidth, window.innerHeight ); // Configurar tamaño del render
+
+// Añadir render al HTML
 document.body.appendChild( renderer.domElement );
+
+// ------------------------------------------------
+// FUN STARTS HERE
+// ------------------------------------------------
+
+// Cubo inicial
+var geometry = new THREE.BoxGeometry( 3, 0.8, 3 );
+var material = new THREE.MeshLambertMaterial( { color: 0xfb8e00 } );
+var cube = new THREE.Mesh( geometry, material );
+//cube.position.set(0, 0, 0);
+//cube.rotateX(10);
+//cube.rotateY(Math.PI/4);
+scene.add( cube ); // Añadir el cubo a la escena
 
 //Iluminación
 const a_light = new THREE.AmbientLight(0xffffff, 0.6); // Luz ambiente
 scene.add(a_light);
-
-const d_light = new THREE.DirectionalLight(0xffffff, 0.6); //luz direccional
+const d_light = new THREE.DirectionalLight(0xffffff, 0.6); // Luz direccional
 d_light.position.set(10, 20, 0);
 scene.add(d_light);
 
-//Cubo
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-cube.position(0, 0, 0);
-scene.add( cube );
+// Render Loop
+var render = function () {
+  requestAnimationFrame( render );
 
-camera.position.z = 5;
+  //cube.rotation.y += 0.01;
 
-//Rendering
-function animate() {
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
-}
-animate();
+  // Render la escena
+  renderer.render(scene, camera);
+};
 
+render();
