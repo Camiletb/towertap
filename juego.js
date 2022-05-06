048c97c6f79f54f7a91d74387b25707d3fd9ecfb
Camiletb <caena@alumni.uv.es>
2022-05-06 12:44:51 +0200

    add lights

--- juego.js
+++ juego.js
@@ -6,14 +6,22 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+//Iluminación
+const a_light = new THREE.AmbientLight(0xffffff, 0.6); // Luz ambiente
+scene.add(a_light);
+
+const d_light = new THREE.DirectionalLight(0xffffff, 0.6); //luz direccional
+d_light.position.set(10, 20, 0);
+scene.add(d_light);
+
 //Cubo
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
-cube.position(0, 5, 0);
+cube.position(0, 0, 0);
 scene.add( cube );
 
-camera.position.z = 10;
+camera.position.z = 5;
 
 //Rendering
 function animate() {
